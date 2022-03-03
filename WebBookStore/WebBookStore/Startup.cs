2f76a75c33de0d278d7cb0fd207077a0c243b6b5
nitishkesarwani01 <nitish.kesarwani01@nagarro.com>
2022-03-03 11:50:27 +0530

    added mvc to application

--- WebBookStore/WebBookStore/Startup.cs
+++ WebBookStore/WebBookStore/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -22,7 +23,8 @@ namespace WebBookStore
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            services.AddControllersWithViews();
+            //services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,8 +47,10 @@ namespace WebBookStore
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapRazorPages();
+                endpoints.MapDefaultControllerRoute();
+            
             });
+            
         }
     }
 }
